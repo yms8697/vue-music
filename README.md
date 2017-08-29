efacaeb8a6a6736b7e841ef25284a9bdb6a99ff7
yms8697 <869735729@qq.com>
2017-08-29 21:20:18 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# vue-music
-> 尝试用vue全家桶开发音乐播放器
-+ [数据来源](https://github.com/Binaryify/NeteaseCloudMusicApi)
+# 欢迎使用 Cmd Markdown 编辑阅读器
+
+# 山东pm2.5展示平台
+用爬虫爬取网上的pm2.5数据进行展示，并用反距离加权插值法，推算济南pm2.5浓度分布情况，渲染成热力图
+### 技术栈
++ vue
++ koa
++ bootstrap
++ echarts
++ 百度地图API
+### 功能介绍
++ 数据查询
++ 变化趋势分析
++ 插值分析
+### 效果图
+![image](https://github.com/yms8697/pm2.5/raw/master/img/1.png)
+![image](https://github.com/yms8697/pm2.5/raw/master/img/2.png)
+![image](https://github.com/yms8697/pm2.5/raw/master/img/3.png)
+![image](https://github.com/yms8697/pm2.5/raw/master/img/4.png)
+![image](https://github.com/yms8697/pm2.5/raw/master/img/5.png)
---
+++ app.js
@@ -0,0 +1,50 @@
+var koa = require('koa');
+var controller=require('koa-route');
+var echarts = require('echarts');
+var app = koa();
+var qs=require('querystring');
+var views=require('co-views');
+var render=views('./view',{
+	map:{html:'ejs'}
+});
+var service=require('./service/webAppService.js');
+//引入静态文件中间件
+var koa_static=require('koa-static-server');
+app.use(koa_static({
+	rootDir:'./static/',
+	rootPath:'/static/',
+	maxage:0
+}));
+//主页路由
+app.use(controller.get('/',function*(){
+	this.set('Cache-Control','no-cache');
+	this.body =yield render('index');
+}));
+//query页路由
+app.use(controller.get('/query',function*(){
+	this.set('Cache-Control','no-cache');
+	this.body =yield render('query');
+}));
+//分析页路由
+app.use(controller.get('/analyze',function*(){
+	this.set('Cache-Control','no-cache');
+	this.body =yield render('analyze');
+}));
+//实时数据接口
+app.use(controller.get('/ajax/query',function*(){
+	this.set('Cache-Control','no-cache');
+	this.body =service.get_query_data();
+}));
+app.use(controller.get('/ajax/shandong',function*(){
+	this.set('Cache-Control','no-cache');
+	this.body =service.get_shandong_data();
+}));
+app.use(controller.get('/ajax/history',function*(){
+	this.set('Cache-Control','no-cache');
+	this.body =service.get_history_data();
+}));
+app.use(controller.get('/ajax/row',function*(){
+	this.set('Cache-Control','no-cache');
+	this.body =service.get_row_data();
+}));
+app.listen(3001);
